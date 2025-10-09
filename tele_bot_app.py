7139111138d21b44000c00e11853b89f81edc462
edenmaman1 <134480166+edenmaman1@users.noreply.github.com>
2025-10-09 20:07:48 +0300

    Update tele_bot_app.py

--- tele_bot_app.py
+++ tele_bot_app.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from datetime import datetime
+from pathlib import Path
 
 import gspread
 import pandas as pd
@@ -14,7 +15,9 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, Con
 # Setup Google Sheets access
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']  # ,"https://www.googleapis.com/auth/spreadsheets"]
-creds = ServiceAccountCredentials.from_json_keyfile_name('cred.json', scope)
+BASE_DIR = Path(__file__).resolve().parent
+CREDS_PATH = BASE_DIR / "cred.json" 
+creds = ServiceAccountCredentials.from_json_keyfile_name(str(CREDS_PATH), scope)
 client = gspread.authorize(creds)
 
 # Global variables
